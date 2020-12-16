c0e80cafe348289c904faaafe99bfcb7dbb73ba7
Jose Sustaita <josesustaita70@gmail.com>
2020-12-15 22:01:17 -0500

    Added Form state

--- src/components/pages/WorkOrderForm/WorkOrderPage.js
+++ src/components/pages/WorkOrderForm/WorkOrderPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState}  from 'react';
 import 'antd/dist/antd.css';
 
 import {
@@ -6,15 +6,42 @@ import {
     Input,
     Button,
     Select,
-    DatePicker,
   } from 'antd';
 
 
 
 function WorkOrderPage({ LoadingComponent }) {
+  
+
+  const [workOrder, setWorkOrder] = useState([
+    {
+      title: "",
+      date: new Date().toISOString().slice(0,10),
+      priority:"",
+      status:"",
+      description: "",
+    },
+  ]);
 
 
 
+  const handleChange = (e) => {
+    setWorkOrder({ ...workOrder, [e.target.name]: e.target.value });
+  };
+
+  function handleDropdownPriority(value,e) {
+    setWorkOrder({ ...workOrder, priority: value});
+    
+}
+
+function handleDropdownStatus(value,e) {
+  setWorkOrder({ ...workOrder, status: value});
+  
+}
+
+  const handleSubmit = (e) =>{
+    console.warn("added", {workOrder})
+}
 
 
     
@@ -32,36 +59,38 @@ function WorkOrderPage({ LoadingComponent }) {
         layout="horizontal"
         initialValues={{
         }}
+
       >
        
-        <Form.Item label="Work Order Title">
-          <Input type="text"  />
+        <Form.Item label="Work Order Title" >
+          <Input type="text" name='title'  onChange={handleChange} />
         </Form.Item>
         <Form.Item label="Date">
-          <DatePicker />
+          <Input type="text" name='date'  value={new Date().toISOString().slice(0,10)} placeholder="yyyy-mm-dd" onChange={handleChange}/>
         </Form.Item>
         <Form.Item label="Priority">
-          <Select >
-            <Select.Option value="Critcal">Critical</Select.Option>
-            <Select.Option value="High">High</Select.Option>
-            <Select.Option value="Medium">Medium</Select.Option>
-            <Select.Option value="Low">Low</Select.Option>
+          
+          <Select name='priority'  onChange={handleDropdownPriority}>
+            <Select.Option value="critcal">Critical</Select.Option>
+            <Select.Option value="high">High</Select.Option>
+            <Select.Option value="medium">Medium</Select.Option>
+            <Select.Option value="low">Low</Select.Option>
 
           </Select>
         </Form.Item>
         <Form.Item label="Status">
-          <Select>
-            <Select.Option value="Unassigned">Unassigned</Select.Option>
-            <Select.Option value="InProgress">In Progress</Select.Option>
-            <Select.Option value="PendingReview">Pending Review</Select.Option>
-            <Select.Option value="Completed">Completed</Select.Option>
-            <Select.Option value="Closed">Closed</Select.Option>
+          <Select name='status'  onChange={handleDropdownStatus}>
+            <Select.Option value="unassigned">Unassigned</Select.Option>
+            <Select.Option value="inProgress">In Progress</Select.Option>
+            <Select.Option value="pendingReview">Pending Review</Select.Option>
+            <Select.Option value="completed">Completed</Select.Option>
+            <Select.Option value="closed">Closed</Select.Option>
 
 
           </Select>
         </Form.Item>
         <Form.Item label="Work Order Description">
-          <Input />
+          <Input type="text" name='description' onChange={handleChange}  />
         </Form.Item>
 
         <Form.Item label="Attachments">
@@ -69,7 +98,7 @@ function WorkOrderPage({ LoadingComponent }) {
         </Form.Item>
         
         <Form.Item >
-          <Button>Save</Button>
+          <Button onClick={handleSubmit} >Save</Button>
         </Form.Item>
       </Form>
     </>
