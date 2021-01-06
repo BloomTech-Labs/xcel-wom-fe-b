128c20a72b1d2a99bacff9d986b88b4d91051743
Idong Essien <idong.essien@comcast.net>
2021-01-05 20:28:23 -0500

    added redux store boiler plate code with text/example reducers and actions

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,4 +1,14 @@
 // import all of your reducers into this file, and export them back out. 
 // This allows for the simplification of flow when importing reducers into your actions throughout your app.
 
+// example test imports, etc
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
+import { combineReducers } from 'redux';
 
+const allReducers = combineReducers({
+    counter : counterReducer,
+    isLogged : loggedReducer
+})
+
+export default allReducers;
