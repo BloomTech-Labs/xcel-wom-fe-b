af43cc4566ee00dff299a736333ebc7c2eff1573
Jose Sustaita <josesustaita70@gmail.com>
2021-01-14 22:31:24 -0500

    Dashboard page created
    Dashboard page created and assigned to route /dashboard

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import { LoginPage } from './components/pages/Login';
 import { config } from './utils/oktaConfig';
 import { LoadingComponent } from './components/common';
 import { WorkOrderPage } from './components/pages/WorkOrderForm';
-
+import DashBoard from './components/pages/DashBoard/DashBoard';
 
 ReactDOM.render(
   <Router>
@@ -41,25 +41,24 @@ function App() {
   };
 
   return (
-    
     <Security {...config} onAuthRequired={authHandler}>
-    <Switch>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/implicit/callback" component={LoginCallback} />
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/implicit/callback" component={LoginCallback} />
 
-      {/* any of the routes you need secured should be registered as SecureRoutes */}
-      <SecureRoute
-        path="/"
-        exact
-        component={() => <HomePage LoadingComponent={LoadingComponent} />}
-      />
-      <SecureRoute path="/example-list" component={ExampleListPage} />      
-      <SecureRoute path="/profile-list" component={ProfileListPage} />
-      <SecureRoute path="/work-order" component={WorkOrderPage} />
+        {/* any of the routes you need secured should be registered as SecureRoutes */}
+        <SecureRoute
+          path="/"
+          exact
+          component={() => <HomePage LoadingComponent={LoadingComponent} />}
+        />
+        <SecureRoute path="/example-list" component={ExampleListPage} />
+        <SecureRoute path="/profile-list" component={ProfileListPage} />
+        <SecureRoute path="/work-order" component={WorkOrderPage} />
+        <SecureRoute path="/dashboard" component={DashBoard} />
 
-      
-      <Route component={NotFoundPage} />
-    </Switch>
+        <Route component={NotFoundPage} />
+      </Switch>
     </Security>
   );
 }
