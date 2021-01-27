480d0f32f850a358e335b2b3b6871c22549af4ce
Jose Sustaita <josesustaita70@gmail.com>
2021-01-27 18:32:49 -0500

    fixed auth issue when trying to add comment or mark complete on WOs

--- src/components/common/MaintenanceWOForm.js
+++ src/components/common/MaintenanceWOForm.js
@@ -1,25 +1,61 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useOktaAuth } from '@okta/okta-react';
+import { getAuthHeader } from '../../api/index';
 
-function MaintenanceWOForm({ title, date, priority, status, description, id }) {
+function MaintenanceWOForm({
+  title,
+  date,
+  priority,
+  status,
+  description,
+  id,
+  comments,
+}) {
   const [workOrder, setWorkOrder] = useState({
     comment: '',
-    status: '',
+    workOrder: id,
   });
+
+  const WOurl = `${process.env.REACT_APP_API_URI}/company/1/order/${id}`;
+  const COMurl = `${process.env.REACT_APP_API_URI}/company/1/order/${id}/comments`;
+
+  //Okta auth
+  const { authState } = useOktaAuth();
+  const headers = getAuthHeader(authState);
+
+  const putWO = (authState, workOrder) => {
+    const headers = getAuthHeader(authState);
+    if (!WOurl) {
+      throw new Error('No URL provided');
+    }
+    return axios
+      .put(WOurl, workOrder, { headers })
+      .then(res => JSON.parse(res.data))
+      .catch(err => err);
+  };
+  const postComWO = (authState, workOrder) => {
+    const headers = getAuthHeader(authState);
+    if (!COMurl) {
+      throw new Error('No URL provided');
+    }
+    return axios
+      .post(COMurl, workOrder, { headers })
+      .then(res => JSON.parse(res.data))
+      .catch(err => err);
+  };
 
   const handleChange = e => {
     setWorkOrder({ ...workOrder, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = e => {
-    axios.put(`https://xcel-wom-api-b.herokuapp.com/company/1/order/${id}`, {
-      workOrder,
-    });
+    e.preventDefault();
+    postComWO(authState, workOrder);
   };
   const handleComplete = e => {
-    axios.put(`https://xcel-wom-api-b.herokuapp.com/company/1/order/${id}`, {
-      status: 5,
-    });
+    e.preventDefault();
+    putWO(authState, { status: 5 });
   };
 
   return (
@@ -31,7 +67,7 @@ function MaintenanceWOForm({ title, date, priority, status, description, id }) {
         <p>Status: {status}</p>
         <p>{description}</p>
         <div>
-          <p>Comments:</p>
+          <p>Comments: {comments}</p>
           <input onChange={handleChange} name="comment" />
         </div>
         <button onClick={handleSubmit}>Update Work Order</button>
