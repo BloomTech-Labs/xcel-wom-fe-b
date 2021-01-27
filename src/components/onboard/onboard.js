a0113345e19ba85422b10ee6647e961d623e79ed
JC8747 <jonathan.chiaverini@gmail.com>
2021-01-26 22:08:45 -0500

    Created onboarding component, needs routing

---
+++ src/components/onboard/onboard.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from 'react-native-confirmation-code-field';
+
+import styles from './styles';
+
+const CELL_COUNT = 4;
+
+const onboard = () => {
+  const [value, setValue] = useState('');
+  const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
+
+  return (
+    <SafeAreaView style={styles.root}>
+      <Text style={styles.title}>Underline example</Text>
+      <CodeField
+        ref={ref}
+        {...props}
+        value={value}
+        onChangeText={setValue}
+        cellCount={CELL_COUNT}
+        rootStyle={styles.codeFieldRoot}
+        keyboardType="number-pad"
+        textContentType="oneTimeCode"
+        renderCell={({index, symbol, isFocused}) => (
+          <View
+            onLayout={getCellOnLayoutHandler(index)}
+            key={index}
+            style={[styles.cellRoot, isFocused && styles.focusCell]}>
+            <Text style={styles.cellText}>
+              {symbol || (isFocused ? <Cursor /> : null)}
+            </Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default onboard;
