1837d11fab654401976d8a05a7fe8e3c794be231
Idong Essien <idong.essien@comcast.net>
2021-01-26 23:17:43 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './state/reducers';
 
+import WorkOrderPage from './components/pages/WorkOrderForm/WorkOrderPage';
+import MaintenanceCard from './components/common/MaintenanceCard';
+
 const store = createStore(rootReducer);
 
 ReactDOM.render(
